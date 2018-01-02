86575e392d2426a02184c07adb55f92dfffe35fd
terry <terry@constructioncloud.com.au>
2018-01-03 10:31:47 +1100

    test array schema

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-
+console.log('connect mongoose')
 mongoose.connect(process.env.MONGODB_URI);
 module.exports = {
   mongoose
